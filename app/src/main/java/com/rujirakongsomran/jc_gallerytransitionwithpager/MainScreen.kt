3942287866eebeb47c3e8b232cb3c7a4a386c17c
Rujira Kongsomran <rujirabiwberry@gmail.com>
2023-09-17 11:04:13 +0700

    HorizontalPager pageOffset and imageSize graphicLayer

---
+++ app/src/main/java/com/rujirakongsomran/jc_gallerytransitionwithpager/MainScreen.kt
@@ -0,0 +1,73 @@
+package com.rujirakongsomran.jc_gallerytransitionwithpager
+
+import android.util.Log
+import androidx.compose.animation.core.animateFloatAsState
+import androidx.compose.animation.core.tween
+import androidx.compose.foundation.ExperimentalFoundationApi
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.pager.HorizontalPager
+import androidx.compose.foundation.pager.rememberPagerState
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Scaffold
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateListOf
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.graphicsLayer
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.unit.dp
+import coil.compose.AsyncImage
+import coil.request.ImageRequest
+
+
+@OptIn(ExperimentalMaterial3Api::class, ExperimentalFoundationApi::class)
+@Composable
+fun MainScreen() {
+    val images = remember {
+        mutableStateListOf(
+            "https://cdn.pixabay.com/photo/2017/09/25/13/12/puppy-2785074_1280.jpg",
+            "https://cdn.pixabay.com/photo/2016/02/18/18/37/puppy-1207816_1280.jpg",
+            "https://cdn.pixabay.com/photo/2015/11/17/13/13/puppy-1047521_1280.jpg",
+            "https://cdn.pixabay.com/photo/2018/05/11/08/11/dog-3389729_1280.jpg",
+            "https://cdn.pixabay.com/photo/2017/07/31/21/15/dog-2561134_1280.jpg",
+            "https://cdn.pixabay.com/photo/2014/04/05/11/40/dog-316598_1280.jpg"
+        )
+    }
+
+    val pagerState = rememberPagerState()
+
+    Scaffold(modifier = Modifier.padding(vertical = 48.dp)) { padding ->
+        HorizontalPager(
+            pageCount = images.size,
+            state = pagerState
+        ) { index ->
+            Log.d("TAG", "${pagerState.currentPageOffsetFraction}")
+            val pageOffset = (pagerState.currentPage - index) + pagerState.currentPageOffsetFraction
+            val imageSize by animateFloatAsState(
+                targetValue = if (pageOffset != 0.0f) 0.75f else 1f,
+                animationSpec = tween(durationMillis = 300),
+                label = "image size"
+            )
+            AsyncImage(
+                modifier = Modifier
+                    .fillMaxSize()
+                    .padding(16.dp)
+                    .clip(RoundedCornerShape(16.dp))
+                    .graphicsLayer {
+                        scaleX = imageSize
+                        scaleY = imageSize
+                    },
+                model = ImageRequest.Builder(LocalContext.current)
+                    .data(images[index])
+                    .build(),
+                contentDescription = "Image",
+                contentScale = ContentScale.Crop
+            )
+        }
+    }
+}
